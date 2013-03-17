90d2a07505037ad7fa19c38bcc6fda644128e1d9
kerry95 <95kerry@gmail.com>
2013-03-17 22:06:15 +0800

    ﻿添加了getData和appendPage接口，但还有待修改

--- public/js/view/home.js
+++ public/js/view/home.js
@@ -15,6 +15,37 @@ define([
     initialize: function () {
       this.template = _.template(tpl);
       this.collection = new PostCollection;
+    },
+    getData: function (index, callback) {
+      var options, url;
+
+      options = {
+        success: function (collection) {
+          callback(collection);
+        },
+        error: function (collection, xhr) {
+          console.log('Error: collection fetch ', xhr);
+        }
+      };
+      
+      // Override collection's url
+      url = this.collection.url;
+      if (url.charAt(url.length - 1) !== '/') {
+        url += '/';
+      }
+      options.url = url + index + '/' + PAGE_STEP;
+
+      this.collection.fetch(options);
+    },
+    appendPage: function (pageIndex) {
+      var homeView = this;
+      this.getData(pageIndex, function (postCollection) {
+        postCollection.forEach(function (model) {
+          var postView = new PostView;
+          postView.model = model;
+          homeView.$('#content').append(postView.render().el);
+        });
+      });
     }
   });
 
